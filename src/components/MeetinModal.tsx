e1dff81c7f5c861293a0a5e33176168e833ccbe0
Tashrif-007 <tashrifpro@gmail.com>
2025-03-19 15:15:53 +0600

    meet hook added

--- src/components/MeetinModal.tsx
+++ src/components/MeetinModal.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-// import useMeetingActions from "@/hooks/useMeetingActions";
+import useMeetingActions from "@/hooks/useMeetingActions";
 
 interface MeetingModalProps {
   isOpen: boolean;
@@ -14,7 +14,7 @@ interface MeetingModalProps {
 
 function MeetingModal({ isOpen, onClose, title, isJoinMeeting }: MeetingModalProps) {
   const [meetingUrl, setMeetingUrl] = useState("");
-//   const { createInstantMeeting, joinMeeting } = useMeetingActions();
+  const { createInstantMeeting, joinMeeting } = useMeetingActions();
 
   const handleStart = () => {
     
