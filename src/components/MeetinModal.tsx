039259c48d6fca7ecc3783eedce97b773e1d74eb
Tashrif-007 <tashrifpro@gmail.com>
2025-03-19 15:21:23 +0600

    meeting functionalities added

--- src/components/MeetinModal.tsx
+++ src/components/MeetinModal.tsx
@@ -17,7 +17,15 @@ function MeetingModal({ isOpen, onClose, title, isJoinMeeting }: MeetingModalPro
   const { createInstantMeeting, joinMeeting } = useMeetingActions();
 
   const handleStart = () => {
-    
+    if(isJoinMeeting) {
+      const meetingId = meetingUrl.split("/").pop();
+      if(meetingId) joinMeeting(meetingId);
+    } else {
+      createInstantMeeting();
+    }
+
+    setMeetingUrl("");
+    onClose();
   };
 
   return (
